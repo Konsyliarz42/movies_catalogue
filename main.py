efb16233c5dc22c9ebba48f764106ca6ec209c8a
Tomasz Kordiak <dotomika@gmail.com>
2020-10-13 18:12:17 +0200

    Add new movies lists and new page with informations about movie

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = [1,2,3,4]
-    return render_template("homepage.html", movies=movies)
+    selected    = request.args.get('list_type', "popular")
+    results     = request.args.get('results', "8")
+    types       = ['top_rated', 'upcoming', 'popular', 'now_playing']
+    quantity    = [8, 12, 16, 20]
+    
+    try:
+        results = int(results)
+    except ValueError:
+        results = 8
 
+    if selected not in types:
+        selected = 'popular'
+
+    movies = tmdb_client.get_movies(results, selected)
+    return render_template("homepage.html", movies=movies, selected=selected, types=types, results=results, quantity=quantity)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    movie = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=movie, cast=cast)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tmdb_client.py
@@ -0,0 +1,30 @@
+import requests
+
+API_KEY = "ee51b619aa97bfee8dc90c91dda1954f"
+
+def get_movies_list(type_list):
+    endpoint    = f"https://api.themoviedb.org/3/movie/{type_list}"
+    response    = requests.get(endpoint, params={"api_key":API_KEY})
+
+    return response.json()
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+def get_movies(how_many, type_list):
+    data = get_movies_list(type_list)
+    return data["results"][:how_many]
+
+def get_single_movie(movie_id):
+    endpoint    = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    response    = requests.get(endpoint, params={"api_key":API_KEY})
+
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    response    = requests.get(endpoint, params={"api_key":API_KEY})
+
+    return response.json()["cast"]
+
