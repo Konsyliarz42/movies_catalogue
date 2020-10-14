d4abb00e93da0aec6c8d56829f167eee16f537b7
Tomasz Kordiak <dotomika@gmail.com>
2020-10-14 14:27:56 +0200

    Add writable value how mutch result will be on page

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def homepage():
     selected    = request.args.get('list_type', "popular")
     results     = request.args.get('results', "8")
     types       = ['top_rated', 'upcoming', 'popular', 'now_playing']
-    quantity    = [8, 12, 16, 20]
+    quantity    = [4, 8, 12, 16, 20]
     
     try:
         results = int(results)
@@ -21,11 +21,13 @@ def homepage():
     movies = tmdb_client.get_movies(results, selected)
     return render_template("homepage.html", movies=movies, selected=selected, types=types, results=results, quantity=quantity)
 
+
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
+
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
@@ -33,5 +35,6 @@ def movie_details(movie_id):
     cast = tmdb_client.get_single_movie_cast(movie_id)
     return render_template("movie_details.html", movie=movie, cast=cast)
 
+#================================================================
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -8,19 +8,23 @@ def get_movies_list(type_list):
 
     return response.json()
 
+
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
 
+
 def get_movies(how_many, type_list):
     data = get_movies_list(type_list)
     return data["results"][:how_many]
+
 
 def get_single_movie(movie_id):
     endpoint    = f"https://api.themoviedb.org/3/movie/{movie_id}"
     response    = requests.get(endpoint, params={"api_key":API_KEY})
 
     return response.json()
+    
 
 def get_single_movie_cast(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
